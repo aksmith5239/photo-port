c07bf60cdd1435fe45bec7294fdeb3f385d15c05
aksmith5239 <aklobby@gmail.com>
2020-12-18 07:20:43 -0700

    contact form created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 
 function App() {
@@ -26,7 +27,8 @@ function App() {
       ></Nav>
       <main>
         <div>
-        <Gallery currentCategory={currentCategory}></Gallery>
+          <ContactForm></ContactForm>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
         </div>
       </main>
